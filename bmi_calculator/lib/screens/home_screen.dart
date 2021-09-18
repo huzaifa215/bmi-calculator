dff8d9e2c9cab2bc2a2e2e3ad7958d1e2e99be6b
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-09-19 03:49:10 +0500

    minnor changes

--- bmi_calculator/lib/screens/home_screen.dart
+++ bmi_calculator/lib/screens/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:bmi_calculator/widgets/gender_selection_widget.dart';
 import 'package:bmi_calculator/widgets/height_widget.dart';
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight=80.0;
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -23,6 +25,15 @@ class HomeScreen extends StatelessWidget {
           GenderSelectionWidget(),
           HeightWidget(),
           CounterWidget(),
+          Container(
+            margin: EdgeInsets.only(top: 15),
+            width: double.infinity,
+            height: bottomContainerHeight,
+            decoration: BoxDecoration(
+              color: Color(0xFFEB1555),
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(40),topRight: Radius.circular(40)),
+            ),
+          )
         ],
       ),
     );
